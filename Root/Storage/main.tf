5caf906ab8ec8d9a95a0b17dd78dc5b7ea6f6ff8
domHenin <domhenin.517@gmail.com>
2019-03-29 12:37:22 -0400

    storage build

---
+++ Root/Storage/main.tf
@@ -0,0 +1,17 @@
+#-----storage/main.tf-----
+
+# Create a Random ID
+resource "random_id" "tf_bucket_id" {
+  byte_length = 2
+}
+
+# Create the Bucket
+resource "aws_s3_bucket" "tf_code" {
+  bucket = "${var.project_name}-${random_id.tf_bucket_id.dec}"
+  acl = "private"
+  force_destroy = true #--> need this to automatically delete bucket
+
+  tags {
+    Name = "tf_bucket"
+  }
+}
