c559bdd10bd8fb9d17c646236e08f3971e86770e
domHenin <domhenin.517@gmail.com>
2019-04-02 14:16:45 -0400

    networking root modules

--- Root/main.tf
+++ Root/main.tf
@@ -11,9 +11,21 @@ module "storage" {
   project_name = "${var.project_name}"
 }
 
+# Deploy Networking Resources
+module "networking" {
+  source = "./networking"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip = "${var.access_ip}"
+}
+
 //TODO::
 //create provider aws
 //  region: use variable
 //create module: storage
 //  set source to ./storage
 //  set project name to a variable
+//create module: networking
+//   set source: ./networking
+//   set vpc_cidr: variable
+//   set accessship: variable
--- Root/variables.tf
+++ Root/variables.tf
@@ -6,7 +6,23 @@ variable "aws_region" {}
 # Variable for Project Name
 variable "project_name" {}
 
+# Variable for VPC Cidr
+variable "vpc_cidr" {}
+
+# Variable for Public Cidrs
+variable "public_cidrs" {
+  type = "list"
+}
+
+# Variable for Access IP
+variable "access_ip" {}
+
 //TODO:
 //variable: aws_region
 //variable: project_name
+//variable: vpc_cidr
+//variable: accessip
 //- do not provide values
+//
+//variable public_cidrs
+//  - set type: list
