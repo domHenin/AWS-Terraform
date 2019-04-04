52b4a7363637b18ebf0ae15f5450b4bb8efa3967
domHenin <domhenin.517@gmail.com>
2019-04-04 14:22:57 -0400

    added TODO

--- Root/Compute/variables.tf
+++ Root/Compute/variables.tf
@@ -6,6 +6,12 @@ variable "key_name" {
 }
 
 # Variable
-variable "public_key_auth" {
+variable "public_key_path" {
   default = "/home/ec3-user/.ssh/id_rsa.pub"
 }
+
+//TODO:
+//varibale: key_name
+//  - default: tfkey
+//variable: public_key_path
+//  - default: "/home/ec3-user/.ssh/id_rsa.pub"
