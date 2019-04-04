7a5df3231a853fd54c84cf02f91871e0d8fa0930
domHenin <domhenin.517@gmail.com>
2019-04-04 14:15:40 -0400

    Compute Build

---
+++ Root/Compute/main.tf
@@ -0,0 +1,22 @@
+#-----compute/main.tf-----
+
+# Build AMI
+data "aws_ami" "server_ami" {
+  most_recent = true
+
+  filter {
+    name = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name = "name"
+    values = ["amzn-ami-hvm*-x86_64-gp2"]
+  }
+}
+
+# Build Key Pair
+resource "aws_key_pair" "tf_auth" {
+  key_name = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
