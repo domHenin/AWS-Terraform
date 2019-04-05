1aa42c6e7acad3f2fe93c7276ab5ddb0536b37ee
domHenin <domhenin.517@gmail.com>
2019-04-05 10:29:24 -0400

    inserted commment and block for template file

--- Root/Compute/main.tf
+++ Root/Compute/main.tf
@@ -18,34 +18,35 @@ data "aws_ami" "server_ami" {
 # Build Key Pair
 resource "aws_key_pair" "tf_auth" {
   key_name = "${var.key_name}"
-  public_key = "${file(var.public_key_path)}"
+  public_key = "${var.public_key_path}"
 }
 
+# Build Template File
 data "template_file" "user-init" {
   count = 2
   template = "${file("${path.module}/userdata.tpl")}"
 
   vars {
-    firewall_subnets = "${element("${var.subnet_ips}", count.index)}"
+    firewall_subnets = "${element(var.subnet_ips, count.index)}"
   }
 }
 
 # Build EC2 Instance
 resource "aws_instance" "tf_server" {
-  count = "${var.instance_count}"
-  instance_type = "${var.instance_type}"
+  count = "${var.instacne_count}"
   ami = "${data.aws_ami.server_ami.id}"
+  instance_type = "${var.instance_type}"
 
   tags {
-    Name = "tf_server-${count.index +1}"
+    Name = "${aws_instance.tf_server}-${count.index+1}"
   }
 
   key_name = "${aws_key_pair.tf_auth.id}"
   vpc_security_group_ids = ["${var.security_group}"]
 
-  subnet_id = "${element(var.subnet, count.index)}"
+  subnet_id = "${element(var.subets, count.index)}"
 
-  user_data = "${data.template_file.user-init.*.rendered[cont.index]}"
+  user_data = "${data.templae_file.user-init.*.rendered[cont.index]}"
 }
 
 //TODO:
