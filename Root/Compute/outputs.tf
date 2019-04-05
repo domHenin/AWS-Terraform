e260647b532a28b1d173db418774c8a1e02069d1
domHenin <domhenin.517@gmail.com>
2019-04-05 12:24:24 -0400

    outputs build

--- Root/Compute/outputs.tf
+++ Root/Compute/outputs.tf
@@ -1,1 +1,17 @@
 #-----compute/outputs.tf-----
+
+# Output Server ID
+output "server_id" {
+  value = "${join(", ", aws_instance.tf_server.*.id)}"
+}
+
+# Output Server Ip
+output "server_ip" {
+  value = "${join(", ", aws_instance.tf_server.*.public_ip)}"
+}
+
+//TODO:
+//output: server_id
+//  - set value: ${join(", ", aws_instance.tf_server.*.id)}
+//output: server_ip
+//- set value: ${join(", ", aws_instance.tf_server.*.public_ip)}
--- Root/outputs.tf
+++ Root/outputs.tf
@@ -1,10 +1,52 @@
 #-----root/output.tf-----
 
+
+#-----storage/outputs
 # Output Bucket Name
 output "Bucket Name" {
   value = "${module.storage.bucketname}"
 }
 
+
+#-----networking/output
+# Output Public Subnets
+output "Public Subnets" {
+  value = "${join(", ", module.networking.public_subnets)}"
+}
+
+
+#Output Subnet IPs
+output "Subnet IPs" {
+  value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+# Output Public Security Group
+output "Public Security Group" {
+  value = "${module.networking.public_sg}"
+}
+
+#-----compute/outputs
+
+# Output Public Instance IDs
+output "Public Instance IDs" {
+  value = "${module.compute.server_id}"
+}
+
+# Output Public Instance IPs
+output "Public Instance IPs" {
+  value = "${module.compute.server_ip}"
+}
+
 //TODO::
 //output: Bucket Name
 //  set value to module.bucketname
+//output: Public Subnets
+//  set value: ${join(", ", module.networking.public_subnets)}
+//output: Subnet IPs
+//  set value: "${join(", ", module.networking.subnet_ips)}"
+//output: Public Security Group
+//  set value: reference network modules subnet_ips
+//output: Public Instance IDs
+//  set value: reference compute modules server_id
+//output: Public Instance IPs
+//  set value: reference copute modues server_ip
