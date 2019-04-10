cd40d83fc342b15039d1e3ce8087b877a294e90f
domHenin <domhenin.517@gmail.com>
2019-04-10 13:58:30 -0400

    fixed bug, add TODO for output

--- Root/Networking/outputs.tf
+++ Root/Networking/outputs.tf
@@ -10,7 +10,15 @@ output "public_sg" {
   value = "${aws_security_group.tf_public_sg.id}"
 }
 
-# Output
+# Output Subnet IPs
 output "subnet_ips" {
   value = "${aws_subnet.tf_public_subnet.*.cidr_block}"
 }
+
+//TODO::
+//output: public_subnets
+//  set value: refrence subnet -> .*.id
+//output: public_sg
+//  set value: reference security group -> .id
+//output: subnet_ips
+//  set value: reference subnet -> .*.cider_block
